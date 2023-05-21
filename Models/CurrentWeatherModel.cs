a2e7f026f9d263be6ccf3cfa65f8e9b1944c9a11
Palli Moon <pallimoon@gmail.com>
2023-05-21 16:14:35 +0000

    Cleanup

--- Models/CurrentWeatherModel.cs
+++ Models/CurrentWeatherModel.cs
@@ -2,17 +2,17 @@
 {
     public class CurrentWeatherModel
     {
-        public string? City { get; set; } // TODO: TEMP
+        public string City { get; set; } // TODO: TEMP
 
         public float Precip24Hour { get; set; }
-        public string? SunriseTimeLocal { get; set; }
-        public string? SunsetTimeLocal { get; set; }
+        public string SunriseTimeLocal { get; set; }
+        public string SunsetTimeLocal { get; set; }
         public int Temperature { get; set; }
         public int TemperatureMax24Hour { get; set; }
         public int TemperatureMin24Hour { get; set; }
         public int UvIndex { get; set; }
-        public string? WindDirectionCardinal { get; set; }
+        public string WindDirectionCardinal { get; set; }
         public int WindSpeed { get; set; }
-        public string? WxPhraseLong { get; set; }
+        public string WxPhraseLong { get; set; }
     }
 }
--- Models/LocationModel.cs
+++ Models/LocationModel.cs
@@ -7,7 +7,7 @@
 
     public class Location
     {
-        public string[]? IcaoCode { get; set; }
-        public string[]? Address { get; set; }
+        public string[] IcaoCode { get; set; }
+        public string[] Address { get; set; }
     }
 }
